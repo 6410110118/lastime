01f66eeefbf3dc3a81c28cd40987dab7345d5868
6410110118 <6410110118@psu.ac.th>
2024-07-11 18:37:53 +0700

    ee

--- lib/screens/my_drawer.dart
+++ lib/screens/my_drawer.dart
@@ -18,7 +18,7 @@ class MyDrawer extends StatelessWidget {
               padding: const EdgeInsets.symmetric(vertical: 14, horizontal: 15),
               color: Colors.grey,
               child: Text(
-                'Task Drawer',
+                'Last Time',
                 style: Theme.of(context).textTheme.headlineSmall,
               ),
             ),
@@ -50,19 +50,7 @@ class MyDrawer extends StatelessWidget {
                 );
               },
             ),
-            BlocBuilder<SwitchBloc, SwitchState>(
-              builder: (context, state) {
-                return Switch(
-                    value: state.switchValue,
-                    onChanged: ((newValue) {
-                      if (newValue) {
-                        context.read<SwitchBloc>().add(SwitchOnEvent());
-                      } else {
-                        context.read<SwitchBloc>().add(SwitchOffEvent());
-                      }
-                    }));
-              },
-            )
+            
           ],
         ),
       ),
